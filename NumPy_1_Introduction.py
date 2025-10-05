dc7d1310b9b198430020a6f6de5d54f718a4aedc
Code_Masher <137721678+Era8Coder@users.noreply.github.com>
2025-10-06 00:48:35 +0530

    Create NumPy_1_Introduction.py
    NumPy Introduction

---
+++ NumPy_1_Introduction.py
@@ -0,0 +1,29 @@
+"""
+NumPy is a low level library written in C (and FORTRAN) for high level
+mathematical functions. NumPy cleverly overcomes the problem of running
+slower algorithms on Python by using multidimensional arrays and functions
+that operate on arrays. Any algorithm can then be expressed as a function on
+arrays, allowing the algorithms to be run quickly.
+"""
+
+"""
+  When you have to work closer to HardWare -> That's C 
+  Example: 
+    a) Drivers, Pendrive
+    b) Games, Controllers
+    c) Robots
+    d) Arduino
+    e) Binary Language    --->>> Just above C Language
+  Python -> Made simple => Application Programming Language 
+  Pandas => Use of NumPy
+"""
+
+## C ka speed and Python ka Comfort -> Libraries of Java and Javascript are not as advanced as Python One's
+
+"""
+  Applications of NumPy: 
+    1. A powerful N-dimensional array object
+    2. Sophisticated (broadcasting) functions
+    3. Tools for integrating C/C++ and Fortran code
+    4. Useful linear algebra, Fourier transform, and random number capabilities
+"""
