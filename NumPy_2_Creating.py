d91d92365b1e82e979b49950ed5ba4216f19c543
Code_Masher <137721678+Era8Coder@users.noreply.github.com>
2025-10-06 01:03:11 +0530

    Create NumPy_2_Creating.py
    Numpy_Basics

---
+++ NumPy_2_Creating.py
@@ -0,0 +1,32 @@
+# To understand how to use NumPy Functions
+# Creating numpy/n-d arrays 
+import numpy as np
+
+arr1 = np.array([1,2,3,4,5])
+arr1
+type(arr1)
+
+arr2 = np.array([[1,2,3],[4,5,6]])
+arr2
+
+arr3 = np.zeros((2,3))
+arr3
+
+arr4 = np.ones((3,3))
+arr4
+# By Default 
+
+arr5 = np.identity(5)
+arr5
+
+arr6 = np.arange(10)
+arr6
+
+arr7 = np.arange(5,16,2)
+arr7
+
+arr8 = np.linspace(10,20,10)    # equispaced points between given two numbers (a,b,d) => d is the difference
+arr8
+
+arr9 = arr8.copy()
+arr9
